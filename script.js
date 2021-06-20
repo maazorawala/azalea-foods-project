942c67685c4d298faf24a8de1f4ae4006ef23370
maazorawala <maazmaqbool995@gmail.com>
2021-06-20 22:43:47 +0530

    initial-start

---
+++ script.js
@@ -0,0 +1,12 @@
+/**Loader function */
+document.onreadystatechange = () =>{
+    if(document.readyState != "complete"){
+        document.querySelector("#load").style.visibility="visible";
+        document.body.style.visibility="hidden";
+    }
+    else{
+        document.querySelector("#load").style.visibility="hidden"
+        document.body.style.visibility="visible";
+    }
+}
+/**Loader function */
