77a3fda3894fef43389fc3eae254779ebf876e41
maazorawala <maazmaqbool995@gmail.com>
2021-06-23 22:39:37 +0530

    home-page

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ document.onreadystatechange = () =>{
     }
 }
 /**Loader function */
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
