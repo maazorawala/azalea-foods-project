97b8b989fa3f7e445f0cc3389b85ac35ed6ad3d6
maazorawala <maazmaqbool995@gmail.com>
2021-06-27 22:38:03 +0530

    home-page-full
    home page done with mobile version

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.onreadystatechange = () =>{
         document.querySelector("#load").style.visibility="hidden"
         document.body.style.visibility="visible";
     }
-}
+}   
 /**Loader function */
 
 var slides = document.querySelectorAll('#slides .slide');
@@ -20,3 +20,22 @@ function nextSlide(){
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'slide showing';
 }
+/**opening and closing sidenav */
+
+    const mysideNav = document.querySelector('.mysideNav');
+    const navLinks = document.querySelector('.nav-links');
+    const navbar = document.querySelector(".navbar")
+let openNav = function () {
+    document.getElementById("sideNav").style.width = "100vw";
+    document.querySelector('.nav-links').style.display='block';
+    document.getElementById("sideNav").style.top="0vw";
+}
+
+const closeNav = function() { 
+    document.getElementById("sideNav").style.paddingTop = "0vw"; 
+    document.getElementById("sideNav").style.width = "100vw";
+    document.getElementById("sideNav").style.top="-250vw";
+    document.getElementById("sideNav").style.transition="1s";
+}
+
+/**opening and closing sidenav */
